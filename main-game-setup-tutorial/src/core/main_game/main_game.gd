3b48bd87553a6258bb560e43ea75a6ccd84bf8f5
TheHedonismBot <59512798+Th3HedonismBot@users.noreply.github.com>
2026-05-12 19:43:20 -0400

    Add separate clouds and add subviewport camera

--- main-game-setup-tutorial/src/core/main_game/main_game.gd
+++ main-game-setup-tutorial/src/core/main_game/main_game.gd
@@ -7,6 +7,8 @@ var TEST_LEVEL_02 : PackedScene = load("uid://kikf44gko1yv")
 
 var _current_level : Node = null
 
+@onready var game_display_layer : Control = $GameDisplayLayer
+
 # Game World root nodes
 @onready var level_root  : Node2D = %LevelRoot
 @onready var entity_root : Node2D = %EntityRoot
@@ -21,6 +23,20 @@ func _ready() -> void:
 	Global.main_game = self
 
 	load_level(TEST_LEVEL_02)
+	#load_level(TEST_LEVEL)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"exit_appliation"):
+		print_debug("Qutting game - orphan nodes (if nothing prints everything is ok)")
+		print_orphan_nodes()
+		#get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+		get_tree().quit()
+
+	if event.is_action_pressed(&"zoom_in"):
+		game_display_layer.zoom_in()
+
+	if event.is_action_pressed(&"zoom_out"):
+		game_display_layer.zoom_out()
 
 
 func setup_game() -> void:
